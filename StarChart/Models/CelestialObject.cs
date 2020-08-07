3180c988ecfc1eb7fdea15386c8e12df4558e949
Samuel Lee <samwblee@gmail.com>
2020-08-07 16:23:42 -0400

    created CelestialObject model

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class CelestialObject
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int? OribtedObjectId { get; set; }
+    [NotMapped]    
+    public List<CelestialObject> Satellites { get; set; }
+    public TimeSpan OribtalPeriod { get; set; }
+}
