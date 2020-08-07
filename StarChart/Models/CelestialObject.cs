5e5f8bff17805fc1de009085af30c360fc3a8c06
Samuel Lee <samwblee@gmail.com>
2020-08-07 16:28:49 -0400

    added namespace to CelestialObject

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class CelestialObject
-{
-    public int Id { get; set; }
-    [Required]
-    public string Name { get; set; }
-    public int? OribtedObjectId { get; set; }
-    [NotMapped]    
-    public List<CelestialObject> Satellites { get; set; }
-    public TimeSpan OribtalPeriod { get; set; }
+namespace StarChart.Models {
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OribtedObjectId { get; set; }
+        [NotMapped]    
+        public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OribtalPeriod { get; set; }
+    }
 }
+
